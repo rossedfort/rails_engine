e0aa36d69f6fa593941af79174e7c71ec7f84956
rossedfort <rossedfort@gmail.com>
2016-01-04 18:30:33 -0700

    all merchants endpoint tests passing

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,16 +10,20 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def find
-    if request.query_parameters.keys.first == "id" || "merchant_id"
-      respond_with Item.where("#{request.query_parameters.keys.first}": request.query_parameters.values.first.to_i).first
+    if request.query_parameters.keys.first == "id" || request.query_parameters.keys.first == "merchant_id" || request.query_parameters.keys.first == "unit_price"
+      respond_with Item.where(id: request.query_parameters.values.first.to_i).first
+    elsif request.query_parameters.keys.first == "created_at" || request.query_parameters.keys.first == "updated_at"
+      respond_with Item.where("#{request.query_parameters.keys.first}": request.query_parameters.values.first.to_time).first
     else
       respond_with Item.where("#{request.query_parameters.keys.first} ILIKE ?", request.query_parameters.values.first).first
     end
   end
 
   def find_all
-    if request.query_parameters.keys.first == "id" || "merchant_id"
-      respond_with Item.where("#{request.query_parameters.keys.first}": request.query_parameters.values.first.to_i)
+    if request.query_parameters.keys.first == "id" || request.query_parameters.keys.first == "merchant_id" || request.query_parameters.keys.first == "unit_price"
+      respond_with Item.where(id: request.query_parameters.values.first.to_i)
+    elsif request.query_parameters.keys.first == "created_at" || request.query_parameters.keys.first == "updated_at"
+      respond_with Item.where("#{request.query_parameters.keys.first}": request.query_parameters.values.first.to_time)
     else
       respond_with Item.where("#{request.query_parameters.keys.first} ILIKE ?", request.query_parameters.values.first)
     end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -12,6 +12,8 @@ class Api::V1::MerchantsController < ApplicationController
   def find
     if request.query_parameters.keys.first == "id"
       respond_with Merchant.where(id: request.query_parameters.values.first.to_i).first
+    elsif request.query_parameters.keys.first == "created_at" || request.query_parameters.keys.first == "updated_at"
+      respond_with Merchant.where("#{request.query_parameters.keys.first}": request.query_parameters.values.first.to_time).first
     else
       respond_with Merchant.where("#{request.query_parameters.keys.first} ILIKE ?", request.query_parameters.values.first).first
     end
@@ -20,6 +22,8 @@ class Api::V1::MerchantsController < ApplicationController
   def find_all
     if request.query_parameters.keys.first == "id"
       respond_with Merchant.where(id: request.query_parameters.values.first.to_i)
+    elsif request.query_parameters.keys.first == "created_at" || request.query_parameters.keys.first == "updated_at"
+      respond_with Merchant.where("#{request.query_parameters.keys.first}": request.query_parameters.values.first.to_time)
     else
       respond_with Merchant.where("#{request.query_parameters.keys.first} ILIKE ?", request.query_parameters.values.first)
     end
