502a5aad0e28df0a78367db9d41a7a540bdcc585
rossedfort <rossedfort@gmail.com>
2016-01-04 21:09:10 -0700

    add controller test for customer show and index -add simplecov gem -fix customer fixture data

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   gem 'pry'
+  gem 'simplecov'
 end
 
 group :development do
--- test/controllers/api/v1/customers_controller_test.rb
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class Api::V1::CustomersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index, format: :json
+
+    assert_response :success
+  end
+
+  test "index action returns all the customers" do
+    customer_count = Customer.count
+    get :index, format: :json
+    controller_response = JSON.parse(response.body)
+
+    assert_equal customer_count, controller_response.count
+  end
+
+  test "should get show" do
+    get :show, format: :json, id: 1
+
+    assert_response:success
+  end
+
+  test "show action returns correct customer" do
+    get :show, format: :json, id: 1
+    customer = Customer.find(1)
+    controller_response = JSON.parse(response.body)
+
+    assert_equal customer.id, controller_response["id"]
+    assert_equal customer.first_name, controller_response["first_name"]
+    assert_equal customer.last_name, controller_response["last_name"]
+  end
 end
